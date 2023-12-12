1262179339818885c81eb612e1e80a3597f1cd98
Tye Gar <tyegar8@gmail.com>
2023-12-12 11:20:59 +0700

    third

--- test/App/src/App.vue
+++ test/App/src/App.vue
@@ -1,45 +1,21 @@
-<template>
-  <section v-show="completedAssigments.length">
-    <!-- <h2>Assignment undone({{ assignments.filter(a=>!a.completed).length}}) </h2> -->
-    <h2>Assignment undone({{ completedAssigments.length}}) </h2>
-    <ul>
-      <!-- <li v-for="ret in assignments.filter(a=>!a.completed)" :key="ret.id">{{ ret.name }} -->
-        <li v-for="ret in completedAssigments" :key="ret.id">{{ ret.name }}
-
-        <input type="checkbox" v-model="ret.completed">
-      </li>
-    </ul>
-  </section>
-  <br>
-  <section v-if="unCompletedAssigments.length>0">
-    <h2>Assignment done({{ unCompletedAssigments.length}})</h2>
-    <ul>
-      <li v-for="ret in unCompletedAssigments" :key="ret.id">{{ ret.name }}
-        <input type="checkbox" v-model="ret.completed">
-      </li>
-    </ul>
-  </section>
-</template>
-
-<script >
+<script>
+import { RouterLink, RouterView } from 'vue-router'
 export default {
-  data() {
-    return {
-      assignments: [
-        { id:1,name: 'Chan', completed: false },
-        { id:2,name: 'Panha', completed: false },
-        { id:3,name: 'Pich', completed: false },
-       ]
-    }
-  },
-  computed:{
-    completedAssigments(){
-      return this.assignments.filter(a => a.completed)
-    },
-    unCompletedAssigments(){
-      return this.assignments.filter(a =>!a.completed)
-    }
+  components: {
+    RouterLink,
+    RouterView
   }
 }
 </script>
-<style></style>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1>Hello to vue</h1>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/testcomputed">Test Computed</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
